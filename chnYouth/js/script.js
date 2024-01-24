2228321dad6f3a4082d187c08f554ffb08f65b36
YoungAe Sa <duddo0305@gmail.com>
2024-01-24 10:10:06 +0900

    Async

--- chnYouth/js/script.js
+++ chnYouth/js/script.js
@@ -1,22 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
     // 페이지가 로드되면 실행될 코드
-
-    // header.html을 가져오는 함수
-    async function loadHeader() {
+    async function loadHeadFile() {
         try {
-            const response = await fetch("https://sayoungae.github.io/young/chnYouth/header.html");
+            const response = await fetch("https://sayoungae.github.io/young/chnYouth/incloude/head.html");
             const htmlContent = await response.text();
 
-            // 가져온 내용을 특정 요소에 추가
+            document.querySelector("head").innerHTML = htmlContent;
+        } catch (error) {
+            console.error("head 로드하는 중 오류 발생:", error);
+        }
+    }
+    async function loadSkipNav() {
+        try {
+            const response = await fetch("https://sayoungae.github.io/young/chnYouth/incloude/skipNav.html");
+            const htmlContent = await response.text();
+
+            document.getElementById("skipNav").innerHTML = htmlContent;
+        } catch (error) {
+            console.error("skipNav 로드하는 중 오류 발생:", error);
+        }
+    }
+   
+    async function loadHeader() {
+        try {
+            const response = await fetch("https://sayoungae.github.io/young/chnYouth/incloude/header.html");
+            const htmlContent = await response.text();
+
             document.getElementById("header").innerHTML = htmlContent;
         } catch (error) {
             console.error("Header를 로드하는 중 오류 발생:", error);
         }
     }
+    async function loadAllmenu() {
+        try {
+            const response = await fetch("https://sayoungae.github.io/young/chnYouth/incloude/allmenu.html");
+            const htmlContent = await response.text();
 
+            document.getElementById("allmenu").innerHTML = htmlContent;
+        } catch (error) {
+            console.error("allmenu 로드하는 중 오류 발생:", error);
+        }
+    }
     async function loadFooter() {
         try {
-            const response = await fetch("https://sayoungae.github.io/young/chnYouth/footer.html");
+            const response = await fetch("https://sayoungae.github.io/young/chnYouth/incloude/footer.html");
             const htmlContent = await response.text();
 
             document.getElementById("footer").innerHTML = htmlContent;
@@ -24,7 +51,10 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("footer 로드하는 중 오류 발생:", error);
         }
     }
-    // loadHeader 함수 호출
+    //함수 호출
+    loadHeadFile();
+    loadSkipNav();
     loadHeader();
+    loadAllmenu();
     loadFooter();
 });
